701948c378dce8e74095c39709a0c3236d576966
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-05-20 15:58:55 +0900

    [Gold V] Title: 주사위 쌓기, Time: 32 ms, Memory: 70140 KB -BaekjoonHub

--- 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.swift
+++ 백준/Gold/2116. 주사위 쌓기/주사위 쌓기.swift
@@ -1,5 +1,4 @@
 
-
 var N = Int(readLine()!)! // 주사위 개수
 
 var arr : [[Int]] = []
@@ -7,30 +6,40 @@ for i in 0..<N{
     var M = readLine()!.split(separator: " ").map{ Int($0)! }
     arr.append(M)
 }
+/*
+ 5
+ 2 3 1 6 5 4
+ 3 1 2 4 6 5
+ 5 6 4 1 3 2
+ 1 3 6 2 4 5
+ 4 1 6 5 2 3
+ */
 
-func solution (bottom: Int, dice: [Int]) -> (Int, Int)? {
-    var idx = dice.firstIndex { $0 == bottom }
-    if idx == 0{ // A
-        return (dice[5], max(dice[1],dice[2],dice[3],dice[4]))
-    }else if idx == 1{ // B
-        return (dice[3], max(dice[0], dice[2], dice[4],dice[5]))
-    }else if idx == 2{ // C
-        return (dice[4], max(dice[0], dice[1], dice[3],dice[5]))
-    }else if idx == 3{ // D
-        return (dice[1], max(dice[0], dice[2], dice[4],dice[5]))
-    }else if idx == 4{ // E
-        return (dice[2], max(dice[0], dice[1], dice[3],dice[5]))
-    }else if idx == 5{ // F
-        return (dice[0], max(dice[1], dice[2], dice[3],dice[4]))
+func solution(bottom: Int, dice: [Int]) -> (Int, Int)? {
+    guard let idx = dice.firstIndex(of: bottom) else {
+        return nil
     }
-    return nil
+    // A->F idx 0 ->> 5
+    // B->D idx 1 ->> 3
+    // C->E idx 2 ->> 4
+    let compareIndex = [5, 3, 4, 1, 2, 0]
+    
+    let top = dice[compareIndex[idx]]
+    var maxSide = 0
+    for i in 0..<dice.count {
+        if i != idx && i != compareIndex[idx] {
+            maxSide = max(maxSide, dice[i])
+        }
+    }
+
+    return (top, maxSide)
 }
 var result = 0
 for k in 0..<6{
     
-    guard let temp = solution(bottom: arr[0][k], dice: arr[0]) else { break }
-    var top = temp.0
-    var total = temp.1
+    guard let first = solution(bottom: arr[0][k], dice: arr[0]) else { break }
+    var top = first.0
+    var total = first.1
     
     for j in 1..<N{
         guard let next = solution(bottom: top, dice: arr[j]) else { break }
